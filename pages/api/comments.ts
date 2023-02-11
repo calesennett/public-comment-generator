4ef26fde7f65788921ecc9fb770aaa82b0892b00
calesennett <calesennett@gmail.com>
2023-02-11 13:43:07 -0800

    use Vercel edge runtime

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -1,21 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration, OpenAIApi } from 'openai'
+import fetchAdapter from '@vespaiach/axios-fetch-adapter'
+
+export const config = {
+  runtime: 'edge',
+}
 
 const configuration = new Configuration({
   apiKey: process.env.OPEN_AI_SECRET_KEY,
+  baseOptions: {
+    adapter: fetchAdapter
+  }
 })
 const openai = new OpenAIApi(configuration)
 
-const commentsHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const prompt = `Write a 2 minute public comment ${req.body.commentPosition} ${req.body.issue}`
+const commentsHandler = async (req: Request) => {
+  const { commentPosition, issue } = (await req.json()) as {
+    commentPosition?: string;
+    issue?: string;
+  };
+  const prompt = `Write a 2 minute public comment ${commentPosition} ${issue}`
 
   const completion = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt,
-    max_tokens: 512,
+    max_tokens: 512
   })
 
-  res.status(200).json({ comment: completion.data.choices[0].text })
+  return new Response(
+    JSON.stringify({
+      comment: completion.data.choices[0].text
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
+  )
 }
 
 export default commentsHandler
